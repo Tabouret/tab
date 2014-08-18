872ab56fba1d060e93cc24c14d12907ca3c9b9e7
Philibert OLLIER <philibert.ollier@gmail.com>
2014-08-18 23:21:47 +0200

    ft_check_empty_square
    fonction ok
    Adding prototypes to header

--- includes/ft_header.h
+++ includes/ft_header.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_header.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elacombe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pollier <pollier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/18 15:46:38 by elacombe          #+#    #+#             */
-/*   Updated: 2014/08/18 17:56:58 by elacombe         ###   ########.fr       */
+/*   Updated: 2014/08/18 23:20:48 by pollier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,9 @@ void		ft_putchar(char c);
 void		ft_putstr(char *str);
 void		ft_putnbr(int x);
 char		**ft_make_tab(char *str, int x, int y);
+int			ft_check_empty_square (char **tab,int offset_x, int offset_y, int size, char empty_char);
+int			ft_biggest_offset_x (int x, int y);
+int			ft_biggest_offset_y (int x, int y);
+int			ft_biggest_square (int x, int y);
 
 #endif
